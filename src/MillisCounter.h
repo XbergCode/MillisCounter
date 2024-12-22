f5070b62b603bf1b1f6b0bd979e4e9b346074aaf
XbergCode <105010135+XbergCode@users.noreply.github.com>
2024-12-22 03:59:50 +0000

    Update MillisCounter.h
    V: 1.1.0

--- src/MillisCounter.h
+++ src/MillisCounter.h
@@ -21,72 +21,91 @@
 */
 //    FILE: MillisCounter.h
 //  AUTHOR: XbergCode
-// VERSION: 1.0.0
+// VERSION: 1.1.0
 // PURPOSE: Arduino library to count up and down time using the millis() function.
 //     URL: https://github.com/XbergCode/MillisCounter
 
 // History.
 // Version: -  Date:       -  Changes:
+// 1.1.0    -  18/12/2024  -  Changed from static to non static, added void countUp & countDown, added getCountDownIsDone.
 // 1.0.0    -  10/08/2023  -  Initial.
 
 
 #pragma once
-#define MILLISCOUNTER_VERSION    (F("1.0.0\n"))   // Version
+
+// Include The Arduino Header
+#ifdef ARDUINO                                                  // If We Are Using Arduino
+#if !defined(ARDUINO_H)                                         // If The Library Has Not Been Included
+#include <Arduino.h>                                            // Arduino Library
+#endif
+#endif
+
+#define MILLISCOUNTER_VERSION    (F("1.1.0\n"))                 // Version
 
 class MillisCounter {
-  public:                                         // Public
+  public:                                                       // Public
     
-    MillisCounter();                              // Set the class call name
+    // Object Constructor.
+    MillisCounter();                                            // Set the object call name
 
-    // Reset & Status
-    static void reset(void);                      // Reset The Counter
-    static bool isRunning(void);                  // Status Of The Counter
+    // Reset & Status.
+    void reset(void);                                           // Reset the counter
+    bool isRunning(void);                                       // Status of the counter
     
     // Count up.
-    static char* getCountUp(void);                           // Call the count up function
-    // Count down.
-    static char* getCountDown(const uint32_t _COUNTSECONDS); // Call the count down function
-    static char* getCountDown(const uint8_t _SECONDS, const uint8_t _MINUTES, const uint8_t _HOURS = 0, const uint16_t _DAYS = 0U, const uint8_t _YEARS = 0);
+    void countUp(void);                                         // Call the count up function
+    char* getCountUp(void);                                     // Get - Count up (returns: string)
     
+    // Count down.
+    void countDown(const uint32_t _COUNTSECONDS);               // Call the count down function
+    void countDown(const uint8_t _SECONDS, const uint8_t _MINUTES, const uint8_t _HOURS = 0, const uint16_t _DAYS = 0U, const uint8_t _YEARS = 0);
+    char* getCountDown(const uint32_t _COUNTSECONDS);           // Get - Count down (returns: string)
+    char* getCountDown(const uint8_t _SECONDS, const uint8_t _MINUTES, const uint8_t _HOURS = 0, const uint16_t _DAYS = 0U, const uint8_t _YEARS = 0);
+    bool getCountDownIsDone(void) { return _countDownIsDone; }  // Get - Count down is done
+
     // Get the time.
-    static const uint8_t   getYears();            // Get - Years
-    static const uint8_t   getMonths();           // Get - Months
-    static const uint16_t  getDays();             // Get - Days
-    static const uint8_t   getHours();            // Get - Hours
-    static const uint8_t   getMinutes();          // Get - Minutes
-    static const uint8_t   getSeconds();          // Get - Seconds
-    static const uint16_t  getTotalDays();        // Get - Total Days
-    static const uint32_t  getTotalSeconds();     // Get - Total Seconds
+    const uint8_t   getYears() { return _years; }               // Get - Years
+    const uint8_t   getMonths() { return _months; }             // Get - Months
+    const uint16_t  getDays() { return _days; }                 // Get - Days
+    const uint8_t   getHours() { return _hours; }               // Get - Hours
+    const uint8_t   getMinutes() { return _minutes; }           // Get - Minutes
+    const uint8_t   getSeconds() { return _seconds; }           // Get - Seconds
+    const uint16_t  getTotalDays() { return _totaldays; }       // Get - Total Days
+    const uint32_t  getTotalSeconds() {                         // Get - Total Seconds
+      return (_totaldays * 86400UL) + (_hours * 3600UL) + (_minutes * 60UL) + _seconds;
+    }
       
-  private:                                        // Private
+  private:                                                      // Private
   	
     // C string.
-    static char _c_buffer[];                      // Buffer for the time string
-    static const uint8_t _c_bufferSize;           // Buffer size
+    static char _c_buffer[];                                    // Buffer for the time string
+    static const uint8_t _c_bufferSize = 38;                    // Buffer size
 
     // Time.
-    static uint8_t  _years;                       // Time - Years      (0-136)
-    static uint8_t  _months;                      // Time - Months     (0-11)
-    static uint16_t _days;                        // Time - Days       (0-364)
-    static uint8_t  _hours;                       // Time - Hours      (0-23)
-    static uint8_t  _minutes;                     // Time - Minutes    (0-59)
-    static uint8_t  _seconds;                     // Time - Seconds    (0-59)
-    static uint16_t _totaldays;                   // Time - Total Days (0-49710)
+    uint8_t  _years     = 0;                                    // Time - Years      (0-136)
+    uint8_t  _months    = 0;                                    // Time - Months     (0-11)
+    uint16_t _days      = 0U;                                   // Time - Days       (0-364)
+    uint8_t  _hours     = 0;                                    // Time - Hours      (0-23)
+    uint8_t  _minutes   = 0;                                    // Time - Minutes    (0-59)
+    uint8_t  _seconds   = 0;                                    // Time - Seconds    (0-59)
+    uint16_t _totaldays = 0U;                                   // Time - Total Days (0-49710)
     
     // Bools to change day and month.
-    static bool _b_newDay;                        // If its okay to change the day
-    static bool _b_shortMonth;                    // Short or Long month
+    bool _b_newDay = false;                                     // If its okay to change the day
+    bool _b_shortMonth = true;                                  // Short or Long month
 
     // Start mark, start time, total time, rollover time & status.
-    static bool _setTheTime;                      // Get the start millis time once per run
-    static uint32_t _startMillis;                 // Save the start millis time
-    static uint32_t _totalSeconds;                // Save the total seconds
-    static uint32_t _rolloverSeconds;             // Save the total seconds if millis rollsover's
-    static uint32_t _lastMILLIS;                  // Monitor Millis Rollover
-    static bool _counterIsRunning;                // If The Counter Is Running
+    bool _setTheTime = true;                                    // Get the start millis time once per run
+    uint32_t _startMillis = 0UL;                                // Save the start millis time
+    uint32_t _totalSeconds = 0UL;                               // Save the total seconds
+    uint32_t _rolloverSeconds = 0UL;                            // Save the total seconds if millis rollsover's
+    uint32_t _lastMILLIS = 0UL;                                 // Monitor Millis Rollover
+    bool _counterIsRunning = false;                             // If The Counter Is Running
+    bool _countDownIsDone = false;                              // Flag If Count Down Is Done
 
-    // Count down
-    static int64_t roundTo(const int64_t IN);     // Round Milliseconds
+    // Count down roundTo.
+    int64_t roundTo(const int64_t IN);                          // Round Milliseconds
 };
+
 
 // End Of File.
